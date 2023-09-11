735f162ecaa6f346c2294c836b2cfdcf209d90e4
Tobrown3 <saifhusnain@gmail.com>
2023-09-11 09:59:05 -0600

    Added raw script get request

--- requestsverion.py
+++ requestsverion.py
@@ -3,8 +3,12 @@ import requests as rq
 # Print request version
 def main():
     print(rq.__version__)
+    # Get google
     response = rq.get("https://www.google.com")
     print(response)
+    # Get raw python script and print the text
+    raw_script_res = rq.get("https://raw.githubusercontent.com/2Bronze/CMPUT404LAB1/main/requestsverion.py")
+    print(raw_script_res.text)
     
 if __name__ == "__main__":
     main()
