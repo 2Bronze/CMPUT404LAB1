9283e8fd177fe1a7ddd3e574b0aff8e91613cac1
Tobrown3 <saifhusnain@gmail.com>
2023-09-11 09:49:23 -0600

    Added get from google

--- requestsverion.py
+++ requestsverion.py
@@ -3,6 +3,8 @@ import requests as rq
 # Print request version
 def main():
     print(rq.__version__)
+    response = rq.get("https://www.google.com")
+    print(response)
     
 if __name__ == "__main__":
     main()
